7c365f91aae5b10da15e79414ac2fd690d1cdf34
me <gabrielsalesaraujo12@gmail.com>
2026-03-03 17:12:39 -0300

    New style add

--- syshelp/main.py
+++ syshelp/main.py
@@ -1,10 +1,15 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,request
 
 app = Flask(__name__)
 
-@app.route('/login')
+@app.route('/login',methods=['GET','POST'])
 def login():
-    return render_template('/login.html')
+    email = request.form.get('email')
+    senha = request.form.get('senha')
+    if email == 'admin@gmail.com' and senha == 'admin':
+        return render_template('/index.html')
+    else:
+        return render_template('/login.html')
 
 @app.route('/')
 def index():
