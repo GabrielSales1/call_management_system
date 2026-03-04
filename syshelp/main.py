9f56314ec823c48b6fcea0cde914b1ff623fb083
me <gabrielsalesaraujo12@gmail.com>
2026-03-03 22:00:24 -0300

    Syshelp updated, new forms and pages added

--- syshelp/main.py
+++ syshelp/main.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template,request
 
 app = Flask(__name__)
 
+#login "auth"
 @app.route('/login',methods=['GET','POST'])
 def login():
     email = request.form.get('email')
@@ -14,6 +15,31 @@ def login():
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/abrir-chamado')
+def abrirChamado():
+    return render_template('chamados/abrir_chamado.html')
+
+@app.route('/atribuir')
+def atribuirChamado():
+    return render_template('chamados/atribuir_chamado.html')
+
+@app.route('/encerrar')
+def encerrarChamado():
+    return render_template('chamados/encerrar.html')
+
+@app.route('/historico')
+def historicoChamado():
+    return render_template('chamados/historico.html')
+
+@app.route('/listar-chamado')
+def listarChamado():
+    return render_template('chamados/listar_chamado.html')
+
+@app.route('/status')
+def statusChamado():
+    return render_template('chamados/status.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
